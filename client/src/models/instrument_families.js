268ec8f501a96f594e930d9e30bf3814e30c235d
PammyLo <pamela@tuminds.com>
2019-10-27 19:12:46 +0000

    refactor to export/import started

--- client/src/models/instrument_families.js
+++ client/src/models/instrument_families.js
@@ -1,6 +1,8 @@
-const PubSub = require('../helpers/pub_sub.js');
+// const PubSub = require('../helpers/pub_sub.js');
+import PubSub from '../helpers/pub_sub.js';
 
-class InstrumentFamilies {
+
+export default class InstrumentFamilies {
     constructor (data) {
       this.data = data;
     }
@@ -18,23 +20,5 @@ class InstrumentFamilies {
     };
 };
 
-
-// const InstrumentFamilies = function (data) {
-//   this.data = data;
-// };
-//
-// InstrumentFamilies.prototype.bindEvents = function () {
-//   PubSub.publish('InstrumentFamilies:data-ready', this.data);
-//
-//   PubSub.subscribe('SelectView:change', (evt) => {
-//     const selectedIndex = evt.detail;
-//     this.publishFamilyDetail(selectedIndex);
-//   });
-// };
-//
-// InstrumentFamilies.prototype.publishFamilyDetail = function (selectedIndex) {
-//   const selectedFamily = this.data[selectedIndex];
-//   PubSub.publish('InstrumentFamilies:selected-family-ready', selectedFamily)
-// };
-
-module.exports = InstrumentFamilies;
+// module.exports = InstrumentFamilies;
+// export default InstrumentFamilies;
